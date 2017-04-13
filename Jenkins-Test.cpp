32c7b2493a7d7baf95b178f50a9ec713f4b6df04
DominikGraeser <dominik.graeser@ceteq.de>
2017-04-13 10:39:36 +0200

    Add cpp via web

---
+++ Jenkins-Test.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Jenkins-Test.cpp
+// Author      : Dominik Gräser
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int foo(void){
+	return 5;
+}
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	(void) foo();
+	return 0;
+}
